ea7479f327bbfedf08b9bc57175d26aff34dff47
vbenavi0 <victorvill.0988@gmail.com>
2023-10-20 18:10:57 -0400

    made all calculator buttons functional

--- script.js
+++ script.js
@@ -1,194 +1,129 @@
 let num1;
-let quest = document.getElementById('quest')
-let string1 = "";
-let string2 = "";
+let string0 = ""
+let string1 = "0";
+let string2 = "0";
 let string3 = "";
+let string4 = "";
 let nums = [];
 
 
 function ac(){
     if(document.getElementById("clear").innerHTML === "BC"){
-        document.getElementById("quest").innerHTML = "0";
-        string1 = "";
+        document.getElementById("quest").innerHTML = string4;
+        string0 = string4;
+        string1 = string4;
         string2 = "";
         string3 = "";
+        nums = [];
+        nums.push(string4);
         document.getElementById("clear").innerHTML = "AC"
+        console.log('Clear');
     }
     else{
-        string1 = "";
-        string2 = "";
+        string0 = "";
+        string1 = "0";
+        string2 = "0";
+        string3 = "";
+        string4 = "";
         document.getElementById("quest").innerHTML = "0";
         document.getElementById("up").innerHTML = "";
+        nums = [];
+        console.log('Clear All');
     }
 }
 
-function addLeftP(){
-    string1 += "(";
-    string2 += "(";
+function addVal(val){
+    if(string1 === string4){
+        string0 = val;
+        string1 = val;
+        string2 = val;
+        string3 = "";
+        nums = [];
+        document.getElementById("clear").innerHTML = "BC";
+        console.log("Reset");
+    }
+    else if((document.getElementById("quest").innerHTML === "0" && val != '0')){
+        document.getElementById("clear").innerHTML = "BC";
+        string0 = val;
+        string1 = val;
+        string2 = val;
+    }
+    else if(document.getElementById("quest").innerHTML != "0" && val != '0'){
+        document.getElementById("clear").innerHTML = "BC";
+        string0 += val;
+        string1 += val;
+        string2 += val;
+    }
+    else if(document.getElementById("quest").innerHTML === "0" && val === '0'){
+        document.getElementById("clear").innerHTML = "BC";
+    }
+    else{
+        string0 += val;
+        string1 += val;
+        string2 += val;
+    }
     document.getElementById("quest").innerHTML = string1;
-    if(document.getElementById("quest").innerHTML != "0"){
-        document.getElementById("clear").innerHTML = "BC"
-    }    
 }
 
-function addRightP(){
-    string1 +=")";
-    string2 +=")";
-    document.getElementById("quest").innerHTML = string1;
-    if(document.getElementById("quest").innerHTML != "0"){
-        document.getElementById("clear").innerHTML = "BC"
-    }   
-}
-
-function addDec(){
-    string1 += ".";
-    string2 += ".";
-    document.getElementById("quest").innerHTML = string1;
-    if(document.getElementById("quest").innerHTML != "0"){
-        document.getElementById("clear").innerHTML = "BC"
-    }   
-}
-
-function add0(){
-    string1 += "0";
-    string2 += "0";
-    document.getElementById("quest").innerHTML = string1;
-    if(document.getElementById("quest").innerHTML != "0"){
-        document.getElementById("clear").innerHTML = "BC"
-    }   
-}
-
-function add1(){
-    string1 += "1";
-    string2 += "1";
-    document.getElementById("quest").innerHTML = string1;
-    if(document.getElementById("quest").innerHTML != "0"){
-        document.getElementById("clear").innerHTML = "BC"
-    }   
-}
-
-function add2(){
-    string1 += "2";
-    string2 += "2";
-    document.getElementById("quest").innerHTML = string1;
-    if(document.getElementById("quest").innerHTML != "0"){
-        document.getElementById("clear").innerHTML = "BC"
-    }   
-}
-
-function add3(){
-    string1 += "3";
-    string2 += "3";
-    document.getElementById("quest").innerHTML = string1;
-    if(document.getElementById("quest").innerHTML != "0"){
-        document.getElementById("clear").innerHTML = "BC"
-    }   
-}
-
-function add4(){
-    string1 += "4";
-    string2 += "4";
-    document.getElementById("quest").innerHTML = string1;
-    if(document.getElementById("quest").innerHTML != "0"){
-        document.getElementById("clear").innerHTML = "BC"
-    }   
-}
-
-function add5(){
-    string1 += "5";
-    string2 += "5";
-    document.getElementById("quest").innerHTML = string1;
-    if(document.getElementById("quest").innerHTML != "0"){
-        document.getElementById("clear").innerHTML = "BC"
-    }   
-}
-
-function add6(){
-    string1 += "6";
-    string2 += "6";
-    document.getElementById("quest").innerHTML = string1;
-    if(document.getElementById("quest").innerHTML != "0"){
-        document.getElementById("clear").innerHTML = "BC"
-    }   
-}
-
-function add7(){
-    string1 += "7";
-    string2 += "7";
-    document.getElementById("quest").innerHTML = string1;
-    if(document.getElementById("quest").innerHTML != "0"){
-        document.getElementById("clear").innerHTML = "BC"
-    }   
-}
-
-function add8(){
-    string1 += "8";
-    string2 += "8";
-    document.getElementById("quest").innerHTML = string1;
-    if(document.getElementById("quest").innerHTML != "0"){
-        document.getElementById("clear").innerHTML = "BC"
-    }   
-}
-
-function add9(){
-    string1 += "9";
-    string2 += "9";
-    document.getElementById("quest").innerHTML = string1;
-    if(document.getElementById("quest").innerHTML != "0"){
-        document.getElementById("clear").innerHTML = "BC"
-    }   
-}
-
-function multiply(num1, num2){
-    return num1 * num2;
-}
-
-function mult(){
-    string1 += "×";
-    document.getElementById("quest").innerHTML = string1;
+function operation(op){
     nums.push(string2);
-    nums.push("*");
+    if(op === "×"){
+        nums.push("*");
+        string0 += "×";
+        string1 += "×";
+    }
+    else if(op === "÷"){
+        nums.push("/");
+        string0 += "÷";
+        string1 += "÷";
+    }
+    else if(op === "+"){
+        nums.push("+");
+        string0 += "+";
+        string1 += "+";
+    }
+    else if(op === "-"){
+        nums.push("-");
+        string0 += "-";
+        string1 += "-";
+    }
+    document.getElementById("quest").innerHTML = string1;
     string2 = "";
+    document.getElementById("clear").innerHTML = "BC"
 }
 
-function div(){
-    string1 += "÷";
+function percent(){
+    string0 += '%'
+    string1 += '%'
+    string2 += "*"+String(eval(nums[0]*.01));
     document.getElementById("quest").innerHTML = string1;
-    nums.push(string2);
-    nums.push("/");
-    string2 = "";
-}
-
-function sub(){
-    string1 += "-";
-    document.getElementById("quest").innerHTML = string1;
-    nums.push(string2);
-    nums.push("-");
-    string2 = "";
-}
-
-function add(){
-    string1 += "+";
-    document.getElementById("quest").innerHTML = string1;
-    nums.push(string2);
-    nums.push("+");
-    string2 = "";
 }
 
 function equ(){
-    nums.push(string2);
-    for(let i = 0; i<nums.length; i++){
-        string3 += nums[i];
+    if(string1!=string4){
+        nums.push(string2);
+        for(let i = 0; i<nums.length; i++){
+            string3 += nums[i];
+        }
+        string2 = "";
+        nums = [];
+        console.log(string0);
+        console.log(string3);
+        string4 = (eval(string3));
+        document.getElementById("up").innerHTML = string0 + " = "+ string4;
+        document.getElementById("quest").innerHTML = string4;
+        nums.push(string4);
+        string3="";
+        string1 = string4;
+        string0 = string4;
+        // console.log(string1);
+        for(let i = 0; i<nums.length; i++){
+            console.log(nums[i]);
+        }
+        document.getElementById("clear").innerHTML = "AC"
     }
-    string1 = "";
-    string2 = "";
-    nums = [];
-    console.log(string3);
-    document.getElementById("up").innerHTML = string3 + " = "+ eval(string3);
-    document.getElementById("quest").innerHTML = eval(string3);
-    string3="";
-    console.log(string1);
-    for(let i = 0; i<nums.length; i++){
-        console.log(nums[i]);
+    else{
+        document.getElementById("up").innerHTML = string4;
+        document.getElementById("quest").innerHTML = string4;
     }
 }
